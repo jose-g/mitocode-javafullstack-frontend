2d8b7481287c5eca5edc245dd72c26195ae7dfbf
jose-g <jguerrero74@hotmail.com>
2020-04-05 18:46:20 -0500

    implementa nombre de usuario logueado y rol en componente perfil

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
+  nombreUsuario : string = "Jose Atilio Guerrero Romero";
+  nombreRol : string = "Administrador";
 
   constructor() { }
 
   ngOnInit(): void {
+    const helper = new JwtHelperService();
+    let token = sessionStorage.getItem(environment.TOKEN_NAME);
+    const decodedToken = helper.decodeToken(token);
+    console.log(decodedToken);
+    this.nombreUsuario = decodedToken.user_name;
+    this.nombreRol = decodedToken.authorities;
   }
 
 }
